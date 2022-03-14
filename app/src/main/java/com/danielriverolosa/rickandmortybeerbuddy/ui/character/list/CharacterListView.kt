ade39b13454b2d7acf5f9bbfb8f258d95c855f93
Daniel Rivero <daniel.rivero@innocv.com>
2022-03-14 03:45:31 +0100

    Create header on character list to add search view

--- app/src/main/java/com/danielriverolosa/rickandmortybeerbuddy/ui/character/list/CharacterListView.kt
+++ app/src/main/java/com/danielriverolosa/rickandmortybeerbuddy/ui/character/list/CharacterListView.kt
@@ -1,12 +1,15 @@
 package com.danielriverolosa.rickandmortybeerbuddy.ui.character.list
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.danielriverolosa.domain.entity.Character
+import com.danielriverolosa.rickandmortybeerbuddy.R
 import com.danielriverolosa.rickandmortybeerbuddy.databinding.CharacterListViewBinding
 import com.danielriverolosa.rickandmortybeerbuddy.ui.base.BaseFragment
 import com.danielriverolosa.rickandmortybeerbuddy.ui.character.list.CharacterListEvent.Initialize
@@ -42,6 +45,8 @@ class CharacterListView : BaseFragment<CharacterListViewBinding, CharacterListVi
     private fun configureView() {
         binding.apply {
             characterListView.apply {
+                searchView.typeface = ResourcesCompat.getFont(context, R.font.wubba_lubba_dub_dub)
+                searchViewEditText.typeface = ResourcesCompat.getFont(context, R.font.wubba_lubba_dub_dub)
                 adapter = listAdapter
                 endless { viewModel.onEvent(LoadNextPage) }
             }
